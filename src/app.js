0445fb1ec465c5fb182cd36497780bc1bfe85b3c
neeleshsingh05 <neeleshsingh867@gmail.com>
2024-01-07 22:36:29 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
+
+
+const port = process.env.PORT || 20000;
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.set('view engine', 'hbs');
+
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));// static file index.html
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/weather', (req, res) => {
+    res.render('weather');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMsg:'Oops! Page Not Found'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`listening to the port at ${port}`);
+});
